d6b6a38c7ba57ff2b32e46b03e85d9dfc5ebaa64
Oleksandr Fedorov <ofedoro@softserveinc.com>
2019-10-15 17:20:21 +0300

    Introducing runner result.

--- allthethings/core/runners.py
+++ allthethings/core/runners.py
@@ -1,7 +1,20 @@
 import os
 import subprocess
 
+from dataclasses import dataclass
+
 from .base import BaseCommand
+
+
+@dataclass
+class Result:
+    name: str
+    icon: str = "✅"
+    comment: str = "Ok"
+
+    def set_error(self, text):
+        self.icon = "⛔"
+        self.comment = text
 
 
 class Execute(BaseCommand):
@@ -18,8 +31,30 @@ class Execute(BaseCommand):
         groups = set(self.option("group") or {})
         cmd = self.argument("cmd")
 
+        results = []
         for key, conf in self.list_projects(groups):
             path = self.root / key
-            if path.exists():
-                os.chdir(path)
-            subprocess.run(cmd, shell=True)
+            cwd = path if path.exists() else self.root
+            result = Result(key)
+
+            try:
+                res = subprocess.run(
+                    " ".join(cmd), cwd=cwd, shell=True, check=True, capture_output=True
+                )
+            except subprocess.CalledProcessError as error:
+                resp_raw = error.stderr if error.stderr else error.output
+                result.set_error(resp_raw.decode())
+                out = error.output
+                err = error.stderr
+            else:
+                out = res.stdout
+                err = res.stderr
+
+            print_key = f" { key } "
+            print(f"{ print_key :=^70}\n{ out.decode() }\n{ '='*70 }\n{ err.decode() }\n")
+            results.append(result)
+
+        self.render_table(
+            headers=["", "", ""], rows=[[r.icon, r.name, r.comment] for r in results],
+            style="compact"
+        )

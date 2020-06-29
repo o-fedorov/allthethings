a9fd0a8803592a368f06314e59b5593fd02b0e5b
Oleksandr Fedorov <a.g.fedorof@gmail.com>
2020-06-29 11:52:22 +0300

    Provide env vars with a command.

--- tests/core/test_runners.py
+++ tests/core/test_runners.py
@@ -1,7 +1,21 @@
 """Tests of the core projects management."""
+import difflib
+import re
 from textwrap import dedent
 
+import pytest
+
 from .conftest import execute
+
+
+def assert_re_match(actual, expected):
+    expected = dedent(expected)
+
+    if not re.match(expected, actual, flags=re.MULTILINE | re.DOTALL):
+        report = "".join(
+            difflib.ndiff(actual.splitlines(keepends=True), expected.splitlines(keepends=True))
+        )
+        pytest.fail(report)
 
 
 def test_empty(exec_cmd):
@@ -25,20 +39,36 @@ def test_smoke(exec_cmd, add_cmd):
     )
 
 
+def test_envvars(exec_cmd, add_cmd):
+    execute(add_cmd, "project1")
+    execute(
+        exec_cmd, '-- echo "Name: $ALLTHETHINGS_PROJECT_NAME, path: $ALLTHETHINGS_PROJECT_PATH"'
+    )
+    assert_re_match(
+        exec_cmd.io.fetch_output(),
+        """\
+        ============================== project1 ==============================
+        Name: project1, path: .*/allthethings/tests/project1
+
+        ======================================================================
+
+
+        ✅ project1 Ok
+        """,
+    )
+
+
 def test_failure(exec_cmd, add_cmd):
     execute(add_cmd, "project1")
-    execute(exec_cmd, "-- python -c '1/0'")
+    execute(exec_cmd, "-- >&2 echo Failure; exit 1")
     assert exec_cmd.io.fetch_output() == dedent(
         """\
         ============================== project1 ==============================
 
         ======================================================================
-        Traceback (most recent call last):
-          File "<string>", line 1, in <module>
-        ZeroDivisionError: division by zero
+        Failure
 
 
-        ⛔ project1 Traceback (most recent call last):   File "<string>", line 1, in
-                   <module> ZeroDivisionError: division by zero
+        ⛔ project1 Failure
         """
     )
